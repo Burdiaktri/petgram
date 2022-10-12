184757576f174f5953412ba41cf799bfa699e12b
burdiaktri <burdiaktrinidad33@gmail.com>
2022-10-12 00:07:10 -0300

    first commit

---
+++ src/components/ListOfCategories/index.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+import { Category } from '../Category/index'
+import { List, Item } from './styles'
+import { Loading } from '../Loading/index'
+
+function useCategoriesData () {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(function () {
+    setLoading(true)
+    fetch('https://petgram-burdiaktrinidad.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+        setLoading(false)
+      })
+  }, [])
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => {
+    return (
+
+      <List fixed={fixed}>
+        {loading
+          ? <Loading />
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)}
+      </List>
+    )
+  }
+
+  return (
+
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+
+  )
+}
